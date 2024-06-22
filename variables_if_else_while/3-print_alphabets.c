bc51224f1c2b939fe0e772ad47ed70009a7e5298
Elyaaa001 <gabibovaelina75@gmail.com>
2024-06-22 22:33:06 +0000

    g

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-
-
+ *  * main - Prints the last digit of a randomly generated number and a text
+ *   *        based on its value
+ *    *
+ *     * Return: Always 0 (Success)
+ *      */
 int main(void)
 {
 	char a;
