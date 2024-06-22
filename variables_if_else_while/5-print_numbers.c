aabf9a5348ffaad492ac1cc6cb6554cc05850f33
Elyaaa001 <gabibovaelina75@gmail.com>
2024-06-22 22:03:40 +0000

    g

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,5 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
  *  * main - Entry point
  *   *
@@ -9,20 +8,14 @@
 
 int main(void)
 {
+		int x = 0;
 
-	int n;
-	int x;
-
-		srand(time(0));
-			n = rand() - RAND_MAX / 2;
-				x = n % 10;
-
-				if (x > 5)
-					printf("Last digit of %d is %d and is greater than 5\n", n, x);
-				else
-					if (x == 0)
-						printf("Last digit of %d is %d and is 0\n", n, x);
-					else
-						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+			while (x < 10)
+					{
+								printf("%d", x);
+										x++;
+											}
+				printf("\n");
 					return (0);
 }
+
