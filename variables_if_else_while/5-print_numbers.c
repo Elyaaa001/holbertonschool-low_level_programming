5131e1467e831d9acebf753c0c4f4a30ecad054c
Elyaaa001 <gabibovaelina75@gmail.com>
2024-06-23 09:10:34 +0000

    f

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int x = 0;
+	int i;
 
-			while (x < 10)
-					{
-								printf("%d", x);
-										x++;
-											}
-				printf("\n");
-					return (0);
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
 }
-
