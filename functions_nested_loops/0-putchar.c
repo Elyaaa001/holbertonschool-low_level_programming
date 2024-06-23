f520f8791a45012a9ac600946816e9f1892426ed
Elyaaa001 <gabibovaelina75@gmail.com>
2024-06-23 09:55:09 +0000

    d

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,7 @@
-#include "holberton.h"
-/**
- *  * main - Prints "Holberton" followed by a new line.
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
-{
-		_putchar('H');
-			_putchar('o');
-				_putchar('l');
-					_putchar('b');
-						_putchar('e');
-							_putchar('r');
-								_putchar('t');
-									_putchar('o');
-										_putchar('n');
-											_putchar('\n');
+#include <stdio.h>
+#include "main.h"
 
-												return (0);
+int _putchar(char c)
+{
+    return putchar(c);
 }
