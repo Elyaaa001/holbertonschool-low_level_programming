c57b0265dbb31393f490212d9d02a1eb69047d98
Elyaaa001 <gabibovaelina75@gmail.com>
2024-06-23 12:22:02 +0000

    d

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isalpha - entry point
+ * Description: This function prints the alphabet, in lowercase
+ * @c: entry parametr
+ * main - check the code
+ * Return: Always 0
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	printf("-");
+	}
+
+	return (0);
+}
