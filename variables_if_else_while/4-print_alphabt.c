b85e2886fb1daf68a228c176284ceeade990fd72
Elyaaa001 <gabibovaelina75@gmail.com>
2024-06-23 09:03:35 +0000

    f

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-
-
+ * main - Prints the alphabet without q and e.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		char a;
+	int i;
 
-			for (a = 'a'; a <= 'z'; a++)
-					{
-								if (a == 'q' || a == 'e')
-											{
-															a++;
-																	}
-										putchar(a);
-											}
-				putchar('\n');
-					return (0);
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
