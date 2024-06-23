f05589d14e4938cdd456f3ffd287691501e787dd
Elyaaa001 <gabibovaelina75@gmail.com>
2024-06-23 10:08:15 +0000

    d

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
+#include <time.h>
 
-int _putchar(char c)
+/**
+ * main - determine if number is positive or negative
+ *
+ * Description: program will assign a random number to the variable n each time
+ * it is executed.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-    return putchar(c);
+	int n;
+
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is positive\n", n);
+
+	return (0);
 }
