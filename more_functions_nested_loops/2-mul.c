eeebc9d07fcd097b345588689c14c4da36e60b99
Elyaaa001 <gabibovaelina75@gmail.com>
2024-06-25 12:46:03 +0000

    e

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*mul - function that multipliestwo numbers
+*mul - function that multiplies two numbers
 *@a: first number
 *@b: second number
-*Return: returns result
+*Return: product of a and b
 */
 
 int mul(int a, int b)
