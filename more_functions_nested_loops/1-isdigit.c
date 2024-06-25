778431d88293b639d82cab4b93b25d1b8896eaa1
Elyaaa001 <gabibovaelina75@gmail.com>
2024-06-25 12:17:52 +0000

    e

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,14 @@
 #include "main.h"
-/*
- * _isdigit - function that checks if  character is digit or not 
- * @c: tested character
- * Return: returns 1 if it is uppercase , 0 if not
+/**
+ *_isdigit - function that checks if  character is digit or not 
+ *@c: tested character
+ *Return: returns 1 if it is uppercase , 0 if not
  */
 int _isdigit(int c)
-{if (c >= '0' && c <= '9')
-	return (1);
-	else 
-		return (0);}
+{
+if (c >= '0' && c <= '9')
+return (1);
+else 
+return (0);
+}
+
