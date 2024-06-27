8dd18fedea96e6a0cd63af79729da508e0c331d0
Elyaaa001 <gabibovaelina75@gmail.com>
2024-06-27 11:55:23 +0000

    f

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,22 @@
 #include "main.h"
+
 /**
- * _puts - a function that prints a string
- *@str: string
- * Return: nothing
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
  */
-
-
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int count = 0;
+
+	while (count >= 0)
 	{
-		_putchar(*str++);
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
 	}
-	_putchar('\n');
 }
