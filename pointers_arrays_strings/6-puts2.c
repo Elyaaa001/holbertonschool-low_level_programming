2da22cf0d4b65ff0be7e9e9e1ce1a9873dd13deb
Elyaaa001 <gabibovaelina75@gmail.com>
2024-06-27 12:53:05 +0000

    d

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string we operate from
+ */
+
+void puts2(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c += 2)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+}
