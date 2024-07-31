830cc5f367433fc13eb12fb2ca27b806279ba0c8
Elyaaa001 <gabibovaelina75gmail.com>
2024-07-31 12:20:32 +0400

    1

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,23 +11,21 @@
 size_t print_list(const list_t *h)
 {
 
-	int countofnode = 0;
+   int countofnode = 0;
 
-        while (h != NULL)
-        {
-	       	if (h->str == NULL)
-		{
-	 
-		 printf("[0] (nil)\n");
-	       	}
-       
-		else 
-       		{
-			printf("[%d] %s\n", h->len, h->str); 
-       		}
-	       	countofnode++;
-	       	h = h->next;
-       	}
-	return(countofnode);
+   while (h != NULL)
+   {
+      	if (h->str == NULL)
+	{
+	       	printf("[0] (nil)\n");
+	}
+	else 
+	{
+		printf("[%d] %s\n", h->len, h->str);        
+	}
+	countofnode++;
+	h = h->next;
+   }
+   return(countofnode);
 }
 
