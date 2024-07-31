3f566ce7ec416003eb56e2153ad64fb14f357621
Elyaaa001 <gabibovaelina75gmail.com>
2024-07-31 12:39:29 +0400

    1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * list_len - main
+ * @h: clone of our  linkedlist
+ * Return: length
+ */
+
+size_t list_len(const list_t *h)
+{
+	int length = 0;
+
+	while (h != NULL)
+	{
+		length++;
+		h = h->next;
+	}
+	return (length);
+}
