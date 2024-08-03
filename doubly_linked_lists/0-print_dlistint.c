c4ef61a3a007e18dfa7c72c9bf2afbd1f4d46e7e
Elyaaa001 <gabibovaelina75@gmail.com>
2024-08-03 13:39:36 +0000

    1

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -7,13 +7,14 @@
  */
 
 size_t print_dlistint(const dlistint_t *h)
-{	
+{
 	size_t countnodes = 0;
+
 	while (h)
 	{
 		countnodes++;
 		printf("%d\n", h->n);
-		h = h->next;						
+		h = h->next;
 	}
 	return (countnodes);
 }
