2780ffadf49a9b254d1aa63c5c5c97b1bdc48fef
Elyaaa001 <gabibovaelina75@gmail.com>
2024-08-04 12:30:06 +0000

    1

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -7,16 +7,19 @@
  * Return: the number of bits you would need to flip
  * to get from one number to another
  */
- unsigned int flip_bits(unsigned long int n, unsigned long int m)
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0;
 	int i = 0;
+
 	while (i < 64)
 	{
 		if ((n ^ m) & 1UL << i)
 			count++;
 		i++;
-												}
+
+	}
 	
 	return (count);
 }
