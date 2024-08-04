aa7118e867eefbf58c1e5aeab9b55769cf108702
Elyaaa001 <gabibovaelina75@gmail.com>
2024-08-04 10:25:42 +0000

    1

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - main
+ * @head: dblyptr
+ * @n: var1
+ * Return: backnode
+ */
+
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *th1node = (dlistint_t *) malloc(sizeof(dlistint_t));
+	dlistint_t *th2node:
+
+		
+	th2node = *head;
+	if (th1node == NULL)
+	{
+		return (NULL);						
+	}
+
+	th1node->n = n;
+	th1node->next = NULL;
+
+	if (*head == NULL)							
+	{
+									
+		th1node->prev = NULL;						
+		*head = th1node;
+		return (th1node);
+	}
+	while (th2node->next != NULL)
+										
+	{									
+		th2node = th2node->next;
+		
+	}
+
+	th2node->next = th1node;
+	th1node->prev = th2node;
+
+
+	return (th1node);
+}
